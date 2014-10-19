5b224243c54b25253a9a813086efb1ac3aa2bd88
Alxandr <alxandr@alxandr.me>
2014-10-19 16:42:27 +0200

    Initial commit

---
+++ src/YoloDev.BadCode/BadCodeAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Marker for bad code.
+    /// </summary>
+    [Obsolete("This code is bad, and you should feel bad")]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    public sealed class ಠ_ಠAttribute : Attribute
+    { }
+}
